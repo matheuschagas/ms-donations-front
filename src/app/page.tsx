d42920de422a5e687c2655b92c83493435d2da40
matheusmarrane <matheusmarrane@gmail.com>
2024-05-07 23:00:59 -0300

    add map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,13 @@ const formSchema = z.object({
 	long: z.string().min(2).max(50),
 });
 
-const MapComponentWithNoSSR = dynamic(() => import('../components/Map'), {
-	ssr: false, // This will only render on the client-side
-});
-
 const Page = () => {
 	const [locationFeature, setLocationFeature] = useState(true);
 	const [locationGranted, setLocationGranted] = useState(false);
 	const [location, setLocation] = useState<GeolocationPosition | undefined>();
+	const MapComponentWithNoSSR = dynamic(() => import('../components/Map'), {
+		ssr: false, // This will only render on the client-side
+	});
 
 	useEffect(() => {
 		if (navigator.geolocation) {
@@ -34,21 +33,25 @@ const Page = () => {
 		setLocation(position);
 	};
 	const handleGetHelp = () => {
-		//get location
+		if (locationFeature && locationGranted) {
+		} else {
+		}
 	};
 	const handleHelp = () => {};
 	return (
-		<section className="flex h-[calc(100vh-52px)] w-full flex-col items-center justify-center">
-			<MapComponentWithNoSSR location={location} />
-			<div className="fixed bottom-5 left-0 z-10 flex w-full flex-col items-center justify-center gap-4 p-4 sm:flex-row">
-				<Button onClick={handleHelp} size="lg">
-					Quero ajudar
-				</Button>
-				<Button onClick={handleGetHelp} size="lg">
-					Preciso de ajuda
-				</Button>
-			</div>
-		</section>
+		<>
+			<section className="flex h-[calc(100vh-52px)] w-full flex-col items-center justify-center">
+				<MapComponentWithNoSSR location={location} />
+				<div className="fixed bottom-5 left-0 z-10 flex w-full flex-col items-center justify-center gap-4 p-4 sm:flex-row">
+					<Button onClick={handleHelp} size="lg">
+						Quero ajudar
+					</Button>
+					<Button onClick={handleGetHelp} size="lg">
+						Preciso de ajuda
+					</Button>
+				</div>
+			</section>
+		</>
 	);
 };
 
