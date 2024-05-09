6efa11bf089674c3c81024ffd2bd2f60f3a8b5ba
matheusmarrane <matheusmarrane@gmail.com>
2024-05-09 17:02:22 -0300

    fix build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
 				{metadata.title && <title>{metadata.title.toString()}</title>}
 				<meta
 					name="viewport"
-					content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+					content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
 				/>
 			</head>
 			<body
